765271ce13e094ec7dcc84589a1dc8909af25dfd
George <149611357+aFro95@users.noreply.github.com>
2023-11-17 23:27:03 +0200

    Add files via upload

---
+++ Dice roller.py
@@ -0,0 +1,18 @@
+import random
+from Dice_roller_art import dice_art
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: "))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1, 6))
+
+
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+
+for die in dice:
+    total += die
+print(f"total: {total}")
